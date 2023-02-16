c37c2144c31e37776400898c09fbc9dbdb70d679
Augusto Maza <debianmaza@gmail.com>
2023-02-16 16:39:30 -0300

    update #2 16/02/2023 se generaron los templates index y login

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint, render_template,flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from os import path
-from flask_login LoginManager, login_user, login_requerid, logout_user, current_user
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 db = SQLAlchemy()
 DB_NAME = 'database-vet.db'
@@ -13,26 +13,27 @@ def create_app():
 	app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
 	db.init_app(app)
 
-from .views import views
-from .auth import auth
+	from .views import views
+	from .auth import auth
 
-app.register_blueprint(views, url_prefix='/')
-app.register_blueprint(auth, url_prefix='/')
+	app.register_blueprint(views, url_prefix='/')
+	app.register_blueprint(auth, url_prefix='/')
 
-from .models import User, Pets, Races, Vaccines, Shifts, Pays, ClinicHistory, Doctors
+	from .models import User
+	#, Pets, Races, Vaccines, Shifts, Pays, ClinicHistory, Doctors
 
-create_database(app)
+	create_database(app)
 
-login_manager = Loginmanager()
-login_manager = login_view = 'auth.login'
-login_manager.init_app(app)
+	login_manager = LoginManager()
+	login_manager.login_view = 'auth.login'
+	login_manager.init_app(app)
 
-@login_manager.user_loader
-def load_user(id):
-	return User.query.get(int(id))
+	@login_manager.user_loader
+	def load_user(id):
+		return User.query.get(int(id))
 
 
-return app
+	return app
 
 
 def create_database(app):
